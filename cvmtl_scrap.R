a0b28ad97bee629229ab74b656158c02d26409c0
Qi <owenqiz@users.noreply.github.com>
2020-10-09 19:33:44 -0400

    update scraping form

--- cvmtl_scrap.R
+++ cvmtl_scrap.R
@@ -4,16 +4,16 @@ library(stringr)
 
 url <- 'https://santemontreal.qc.ca/population/coronavirus-covid-19/situation-du-coronavirus-covid-19-a-montreal/'
 
-file_name <- paste0('data/', Sys.Date() - 1, '.csv')
+file_name <- paste0('mtl_data/', Sys.Date() - 1, '.csv')
 
 tbl <- read_html(url) %>% 
        html_nodes("table") %>%
        html_table() %>%
        .[[4]] %>%
-       select(1:2) %>%
+       select(c(1,5)) %>%
        rename('Arrondissements' = 1, 'Cas Confirm¨¦s' = 2) %>%
        mutate('Cas Confirm¨¦s' = str_replace_all(.[[2]], ' ', ''))
 
 tbl[34, 1] <- 'Territoire ¨¤ confirmer'
 
-readr::write_csv(tbl, path = file_name)
+readr::write_csv(tbl, file = file_name)
